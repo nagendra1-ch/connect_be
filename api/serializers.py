f30c247bdab68c6f9b767dfcd6cf8128bcdf0026
nagendra1-ch <chnagendra592@gmail.com>
2025-07-27 20:17:30 +0530

    first commit

--- api/serializers.py
+++ api/serializers.py
@@ -2,10 +2,14 @@ from rest_framework import serializers
 from .models import Post
 from django.contrib.auth.models import User
 
+
 class PostSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)  # show username
+
     class Meta:
         model = Post
         fields = '__all__'
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
